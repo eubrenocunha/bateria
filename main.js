fd099a8c335ec44f030385649c5d3453b3f55888
eubrenocunha <breno.mc19@hotmail.com>
2022-12-02 10:11:23 -0300

    primeiros ajustes

--- main.js
+++ main.js
@@ -3,15 +3,21 @@ function tocaSom(idElementoAudio){
 }
 
 const ListasDeTeclas = document.querySelectorAll('.tecla');
+
 let contador=0
 
 //enquanto
 while(contador<ListasDeTeclas.length){
     ListasDeTeclas[contador].onclick=tocaSom;
 
+    const tecla= ListasDeTeclas[contador];
+    const instrumento=teca.classlist[1];
+
+//tamplade string
+
+    tecla.onclick=function () {
+        tocaSom(idAudio);
+    }
     contador=contador + 1;
-    console.log(contador);
+
 }
-
-
-
